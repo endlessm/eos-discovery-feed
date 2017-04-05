3c902864e4021591eff8f91af8d33a40cd191a6c
Sam Spilsbury <sam@endlessm.com>
2017-04-06 01:08:03 +0800

    main: Sanitize synopsis string and remove uninteresting cruft
    Typically in wikipedia articles we see things like alternate
    pronunciations, translations and even references in the synopsis
    of an article. This is wholly uninteresting when viewed through
    grand central, so just remove it.

--- src/main.js
+++ src/main.js
@@ -315,6 +315,27 @@ function load_style_sheet(resourcePath) {
                                              Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION);
 }
 
+//
+// sanitizeSynopsis
+//
+// Sanitize a provided synopsis provided from an article. Remove references
+// and other uninteresting information. Truncate to a few sentences.
+//
+// @param {string} synopsis - The synopsis to sanitize.
+// @returns {string} A sanitized synopsis.
+function sanitizeSynopsis(synopsis) {
+    // Square brackets with numbers are just references
+    synopsis = synopsis.replace(/\[\d+\]/g, '').trim();
+    // Typically the things found in parens are alternatve pronunciations
+    // or translations.
+    synopsis = synopsis.replace(/\(.*?\)/g, '').trim();
+    // Only show the first two sentences.
+    synopsis = synopsis.split('.').slice(0, 2).join('.') + '.';
+    // Normalize whitespace
+    synopsis = synopsis.replace(/\s+/g, ' ').trim();
+    return synopsis;
+}
+
 function populateGrandCentralModelFromQueries(model, proxies) {
     let remaining = proxies.length;
     model.remove_all();
@@ -331,7 +352,7 @@ function populateGrandCentralModelFromQueries(model, proxies) {
                     response.forEach(function(entry) {
                         model.append(new GrandCentralCardStore({
                             title: entry.title,
-                            synopsis: entry.synopsis,
+                            synopsis: sanitizeSynopsis(entry.synopsis),
                             thumbnail_uri: entry.thumbnail_uri
                         }));
                     });
