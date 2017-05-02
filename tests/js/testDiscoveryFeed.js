3598c57e23d520fad84546c484dd8197376feecb
Sam Spilsbury <sam@endlessm.com>
2017-05-02 10:37:28 +0800

    build: Add support for testing and ESLint

---
+++ tests/js/testDiscoveryFeed.js
@@ -0,0 +1,11 @@
+// test/testDiscoveryFeed.js
+//
+// Copyright (c) 2017 Endless Mobile Inc.
+//
+// This contains some tests for the discovery feed
+/* eslint-env jasmine */
+
+describe('Discovery Feed', function() {
+    it('has a test', function() {
+    });
+});
