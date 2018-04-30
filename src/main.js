8805ba712bf277c5c28e6cad2f9715fe954394b2
Sam Spilsbury <sam@endlessm.com>
2018-05-01 04:39:17 +0800

    main: Explicitly quit the application once we call a content-load func
    https://phabricator.endlessm.com/T22288

--- src/main.js
+++ src/main.js
@@ -210,6 +210,14 @@ function loadKnowledgeAppContent(app,
     }
 
     knowledgeSearchProxy.LoadItemRemote(uri, '', timestamp, function(result, excp) {
+        // Explicitly quit the application once we're done calling
+        // the method on the proxy. The application itself might fail
+        // to load, which means that we would not lose focus and close
+        // implicitly.
+        //
+        // https://phabricator.endlessm.com/T22288
+        Gio.Application.get_default().quit()
+
         if (!excp)
             return;
         logError(excp,
@@ -887,6 +895,17 @@ const DiscoveryFeedAppStoreLinkCard = new Lang.Class({
             shellProxy.LaunchRemote('org.gnome.Software.desktop',
                                     Gtk.get_current_event_time(),
                                     (result, excp) => {
+                                        // Explicitly quit the application
+                                        // once we're done calling
+                                        // the method on the proxy. The
+                                        // application itself might fail
+                                        // to load, which means that we
+                                        // would not lose focus and close
+                                        // implicitly.
+                                        //
+                                        // https://phabricator.endlessm.com/T22288
+                                        Gio.Application.get_default().quit()
+
                                         if (!excp)
                                             return;
                                         logError(excp,
